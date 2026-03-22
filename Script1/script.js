bd891453f488405e590c11f26a82761cfe896199
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2026-03-22 12:08:19 -0300

    Add files via upload

---
+++ Script1/script.js
@@ -0,0 +1,5 @@
+document.getElementById("hw").innerText = "Hello World!"
+
+window.alert("Olá mundo!")
+window.confirm("Você está EndaudiE Agrummgit?")
+window.prompt("Você ta perguntando ou afirmando?")
