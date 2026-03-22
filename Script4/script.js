c8760ff7150bfa07a714b5a754735115f0ab7e60
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2026-03-22 18:01:14 -0300

    frescura

--- Script4/script.js
+++ Script4/script.js
@@ -6,6 +6,7 @@ window.onload = function(){
 
     document.getElementById('title').innerText = `Olá, ${nome}!`
     document.getElementById('qntd').innerText = `Seu nome têm ${nome.trim().length} letras.`
-    document.getElementById('uc').innerText = `${nome.toUpperCase()}. Este é seu nome em letra maiúsculas`
     document.getElementById('lc').innerText = `${nome.toLowerCase()}. Este é seu nome em letras minúsculas`
+    document.getElementById('uc').innerText = `${nome.toUpperCase()}. Este é seu nome em letra maiúsculas`
+    
 }
