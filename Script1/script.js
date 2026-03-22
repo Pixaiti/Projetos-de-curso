8ed116b1408c4bc5c0b533f140ebd6f5a8652e11
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2026-03-22 15:40:54 -0300

    Delete Script1 directory

--- Script1/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("hw").innerText = "Hello World!"
-
-window.alert("Olá mundo!")
-window.confirm("Você está EndaudiE Agrummgit")
-window.prompt("Você ta perguntando ou afirmando?")
