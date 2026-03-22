92ce6a7d8985e17cba05b0eba18f4545d1fe493c
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2026-03-22 16:45:36 -0300

    matematica 3

--- Script3/script.js
+++ Script3/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     const n1 = Number(window.prompt('Digite um número'))
     const n2 = Number(window.prompt('Digite outro número'))  
+    let soma = n1 + n2
     
-    document.getElementById("title").innerText = `O resultado é: ${n1 + n2}`
+    document.getElementById("title").innerText = `O resultado é: ${String(soma)}`
 }
